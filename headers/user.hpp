b6db4da96f984cfeddc70584271627f0541f2da5
mike7d7 <mike7d7@proton.me>
2026-09-23 18:31:53 -0600

    class user: add username attribute

--- headers/user.hpp
+++ headers/user.hpp
@@ -2,18 +2,21 @@
 #define USER_H
 
 #include <cstdint>
+#include <string>
 class User {
 private:
   uint32_t user_id;
+  std::string username;
   double pp_value; // rank_score in db
 
 public:
   User() = default;
-  User(uint32_t user_id, double pp_value)
-      : user_id(user_id), pp_value(pp_value) {};
+  User(std::string username, uint32_t user_id, double pp_value)
+      : username(username), user_id(user_id), pp_value(pp_value) {};
 
   uint32_t get_user_id() const;
   double get_pp_value() const;
+  std::string get_user_info() const;
 };
 
 #endif
--- src/user.cpp
+++ src/user.cpp
@@ -1,4 +1,15 @@
 #include "../headers/user.hpp"
+#include <ostream>
+#include <sstream>
+#include <string>
 
 uint32_t User::get_user_id() const { return user_id; }
 double User::get_pp_value() const { return pp_value; }
+
+std::string User::get_user_info() const {
+  std::stringstream output_string;
+  output_string << "Id: " << user_id << std::endl
+                << "Nombre: " << username << std::endl
+                << "Puntos de rendimiento: " << pp_value << std::endl;
+  return output_string.str();
+}
